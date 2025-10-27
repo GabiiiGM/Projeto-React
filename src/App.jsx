0f7f14128f5e0b234d68c1a888944ec187d6e604
GabiiiGM <gabriellegarciamuller@gmail.com>
2025-10-27 17:12:45 -0300

    feat:Criando os arquivos para um projeto react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from './Nav.jsx'
+import './App.css'
+
+const App =() =>{
+  return(
+    <div className="App">
+      <Nav/>
+      <main className="App-content">
+        <span>Empty</span>
+      </main>
+    </div>
+  )
+}
+
+export default App;
