c749b58989d2713781e2d9ca87a8be6b21eb15ee
GabiiiGM <gabriellegarciamuller@gmail.com>
2025-10-29 15:36:43 -0300

    style: estilizando o carrinho

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -9,13 +9,13 @@ function CartPage({ items, onAddOne, onRemoveOne}) {
       {items.map((item) => (
         <li key={item.id} className="CartPage-item">
           <Item item={item}>
-          <div className="CarItem-controls">
+          <div className="CartItem-controls">
             <button className="CartItem-removeOne"
             onClick={()=>onRemoveOne(item)}>-</button>
 
             <span className="CartItem-count">{item.count}</span>
-            
-            <button className="CarItem-addOne"
+
+            <button className="CartItem-addOne"
             onClick={()=>onAddOne(item)}>+</button>
           </div>
           </Item>
